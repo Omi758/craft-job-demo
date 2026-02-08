e49a2297defcd1b89b46f8dbdf3d5af51581d2e2
mizuho.otani <ruisui@gmail.com>
2026-02-08 21:38:13 +0900

    index.phpに追記しました

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>Craft Job | Web制作会社への転職を最短ルートで</title>
-    <meta name="description" content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
-    <meta name="format-detection" content="telephone=no" />
 
-    <!-- favicon/webclipicon -->
-    <link rel="icon" href="img/favicon.ico" />
-    <link rel="icon" href="img/favicon.svg" type="image/svg+xml" />
-    <link rel="apple-touch-icon" href="img/webclip.png" />
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>Craft Job | Web制作会社への転職を最短ルートで</title>
+  <meta name="description" content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
+  <meta name="format-detection" content="telephone=no" />
 
-    <!-- ogp -->
-    <meta property="og:site_name" content="Craft Job | Web制作会社への転職を最短ルートで" />
-    <meta property="og:url" content="URL" />
-    <meta property="og:type" content="website or article" />
-    <meta property="og:title" content="Craft Job | Web制作会社への転職を最短ルートで" />
-    <meta property="og:description" content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
-    <meta property="og:image" content="URL" />
-    <meta property="og:locale" content="ja_JP" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:description" content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
-    <meta name="twitter:image:src" content="URL" />
+  <!-- favicon/webclipicon -->
+  <link rel="icon" href="img/favicon.ico" />
+  <link rel="icon" href="img/favicon.svg" type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="img/webclip.png" />
 
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+  <!-- ogp -->
+  <meta property="og:site_name" content="Craft Job | Web制作会社への転職を最短ルートで" />
+  <meta property="og:url" content="URL" />
+  <meta property="og:type" content="website or article" />
+  <meta property="og:title" content="Craft Job | Web制作会社への転職を最短ルートで" />
+  <meta property="og:description"
+    content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
+  <meta property="og:image" content="URL" />
+  <meta property="og:locale" content="ja_JP" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:description"
+    content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
+  <meta name="twitter:image:src" content="URL" />
 
-    <!-- css -->
-    <link rel="stylesheet" href="css/style.css" />
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
 
-    <!-- noindex_実案件では削除のこと！！ -->
-    <meta name="robots" content="noindex" />
+  <!-- css -->
+  <link rel="stylesheet" href="css/style.css" />
 
-    <!-- js -->
-    <script type="module" src="js/main.js"></script>
-  </head>
-  <body>
-    <header></header>
-    <main>
-      <h1>コーディングテンプレート</h1>
-    </main>
-    <footer></footer>
-  </body>
+  <!-- noindex_実案件では削除のこと！！ -->
+  <meta name="robots" content="noindex" />
+
+  <!-- js -->
+  <script type="module" src="js/main.js"></script>
+</head>
+
+<body>
+  <header></header>
+  <main>
+    <h1>コーディングテンプレート</h1>
+  </main>
+  <footer></footer>
+</body>
+
 </html>
