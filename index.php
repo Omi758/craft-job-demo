8a8384d375e7e800b6aadad07141ff0cd591eee8
mizuho.otani <ruisui919@gmail.com>
2026-02-08 22:45:30 +0900

    head部分を修正しました

--- index.php
+++ index.php
@@ -4,28 +4,29 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>Craft Job | Web制作会社への転職を最短ルートで</title>
+  <title><?php bloginfo("name"); ?> | <?php bloginfo("description"); ?></title>
   <meta name="description" content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
   <meta name="format-detection" content="telephone=no" />
 
   <!-- favicon/webclipicon -->
-  <link rel="icon" href="img/favicon.ico" />
-  <link rel="icon" href="img/favicon.svg" type="image/svg+xml" />
-  <link rel="apple-touch-icon" href="img/webclip.png" />
+  <link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/img/favicon.png' ); ?>" />
+  <link rel="icon" href="<?php echo esc_url( get_template_directory_uri() . '/img/favicon.svg' ); ?>"
+    type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="<?php echo esc_url( get_template_directory_uri() . '/img/ogp.png' ); ?>" />
 
   <!-- ogp -->
   <meta property="og:site_name" content="Craft Job | Web制作会社への転職を最短ルートで" />
-  <meta property="og:url" content="URL" />
+  <meta property="og:url" content="<?php echo esc_url( home_url( '/' ) ); ?>" />
   <meta property="og:type" content="website or article" />
   <meta property="og:title" content="Craft Job | Web制作会社への転職を最短ルートで" />
   <meta property="og:description"
     content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
-  <meta property="og:image" content="URL" />
+  <meta property="og:image" content="<?php echo esc_url( get_template_directory_uri() . '/img/ogp.png' ); ?>" />
   <meta property="og:locale" content="ja_JP" />
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:description"
     content="制作会社のNo.1掲載数!Web制作会社への転職を、最短ルートで。CraftJobはリモート求人多数、未経験可の求人あり。今旬の求人検索サイトです。" />
-  <meta name="twitter:image:src" content="URL" />
+  <meta name="twitter:image:src" content="<?php echo esc_url( get_template_directory_uri() . '/img/ogp.png' ); ?>" />
 
   <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -33,13 +34,13 @@
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
 
   <!-- css -->
-  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/css/style.css' ); ?>" />
 
   <!-- noindex_実案件では削除のこと！！ -->
   <meta name="robots" content="noindex" />
 
   <!-- js -->
-  <script type="module" src="js/main.js"></script>
+  <script type="module" src="<?php echo esc_url( get_template_directory_uri() . '/js/main.js' ); ?>"></script>
 </head>
 
 <body>
