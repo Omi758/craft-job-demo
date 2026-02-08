d3ac71b6c7e87c72ffd845aa83590beb6d24ecdf
mizuho.otani <ruisui919@gmail.com>
2026-02-08 17:40:54 +0900

    リポジトリ作成しました

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { initializeHamburgerMenu } from "./component/hamburger-menu.js";
+import { initializeHeaderBackgroundToggle } from "./component/header-background-toggle.js";
+
+// 全ての初期化を実行
+initializeHamburgerMenu();
+initializeHeaderBackgroundToggle();
