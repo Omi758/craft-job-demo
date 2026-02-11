35a2be0cfa7c92ed48a63ce2dbe5817f2c1be0b0
mizuho.otani <ruisui@gmail.com>
2026-02-11 20:44:10 +0900

    functions.phpにカスタマイザー設定の記述をしました

--- front-page.php
+++ front-page.php
@@ -81,14 +81,21 @@
     <div class="l-footer-inner l-container">
       <nav class="l-footer-nav">
         <ul class="l-footer-nav-list">
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">ホーム</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">求人を探す</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">人気求人</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">お気に入り</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">就活コラム</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">お問い合わせ</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">プライバシーポリシー</a></li>
-          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/about/' ) ); ?>">運営会社</a></li>
+          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">ホーム</a></li>
+          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/recruit/' ) ); ?>">求人を探す</a></li>
+          <li class="l-footer-nav-item"><a
+              href="<?php echo esc_url( add_query_arg( 'orderby', 'popular', home_url( '/recruit/' ) ) ); ?>">人気求人</a>
+          </li>
+          <li class="l-footer-nav-item"><a
+              href="<?php echo esc_url( add_query_arg( 'view', 'favorite', home_url( '/recruit/' ) ) ); ?>">お気に入り</a>
+          </li>
+          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/column/' ) ); ?>">就活コラム</a></li>
+          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>">お問い合わせ</a></li>
+          <li class="l-footer-nav-item"><a href="<?php echo esc_url( home_url( '/privacy/' ) ); ?>">プライバシーポリシー</a></li>
+          <li class="l-footer-nav-item"><a
+              href="<?php echo esc_url( get_theme_mod( 'craftjob_company_url', 'https://google.com/' ) ); ?>"
+              target="_blank" rel="noopener noreferrer">運営会社</a>
+          </li>
         </ul>
       </nav>
 
