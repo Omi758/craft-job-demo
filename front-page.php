d63f419aac9cbb5038a40970447f24dc716a2e43
mizuho.otani <ruisui@gmail.com>
2026-02-19 20:53:50 +0900

    求人カ―ド（ベース）記述しました。

--- front-page.php
+++ front-page.php
@@ -252,6 +252,61 @@
       </div>
     </article>
     <!-- トップページ用コラムカード end -->
+    <!-- 求人一覧カード_ベース -->
+    <article class="c-card-archive-container">
+      <div class="c-card-archive-image">
+        <img src="<?php echo esc_url( get_template_directory_uri() . '/img/sub/recruite-card-thumbnail@2x.webp' ); ?>"
+          width="1548" height="602" alt="合同会社LIBERA_求人用サムネイル" decoding="async" />
+      </div>
+      <div class="c-card-archive-content">
+        <div class="c-card-archive-header">
+          <img class='c-card-archive-company-logo'
+            src='<?php echo esc_url( get_template_directory_uri() . '/img/common/company-logo@2x.webp' ); ?>'
+            width='120' height='120' alt='合同会社LIBERAロゴ' decoding='async' />
+          <h3 class="c-card-archive-company">合同会社LIBERA</h3>
+        </div>
+        <p class="c-card-archive-copy">【未経験OK】Web制作会社のコーダー募集｜デザインの意図を形にする仕事</p>
+        <ul class="c-card-archive-tags">
+          <li><a href="/">#未経験歓迎</a></li>
+          <li><a href="/">#リモート可</a></li>
+          <li><a href="/">#副業OK</a></li>
+          <li><a href="/">#フレックス勤務</a></li>
+          <li><a href="/">#経験者優遇</a></li>
+          <li><a href="/">#服装自由</a></li>
+          <li><a href="/">#土日休み</a></li>
+          <li><a href="/">#PC至急</a></li>
+        </ul>
+        <div class="c-card-archive-list-container">
+          <dl class="c-card-archive-list">
+            <div class="c-card-archive-list-item">
+              <dt class="c-card-archive-list-title">雇用形態</dt>
+              <dd class="c-card-archive-list-value">正社員</dd>
+            </div>
+            <div class="c-card-archive-list-item">
+              <dt class="c-card-archive-list-title">職種</dt>
+              <dd class="c-card-archive-list-value">コーダー・エンジニア</dd>
+            </div>
+            <div class="c-card-archive-list-item">
+              <dt class="c-card-archive-list-title">地域</dt>
+              <dd class="c-card-archive-list-value">福岡</dd>
+            </div>
+            <div class="c-card-archive-list-item">
+              <dt class="c-card-archive-list-title">年収</dt>
+              <dd class="c-card-archive-list-value">300万円</dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+      <div class="c-card-archive-link-container">
+        <a class="c-card-archive-link-entry" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+          aria-label="【未経験OK】Web制作会社のコーダー募集の詳細を見る">応募する</a>
+        <a class="c-card-archive-link" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+          aria-label="【未経験OK】Web制作会社のコーダー募集の詳細を見る">詳しく見る</a>
+        <a class="c-card-archive-favorite" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+          aria-label="【未経験OK】Web制作会社のコーダー募集の詳細を見る">お気に入り</a>
+
+      </div>
+    </article>
   </div>
   <!-- コンポーネント仮作成 終わり -->
 
