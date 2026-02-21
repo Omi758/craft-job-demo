6cef7c48dc1b1b96461e070d7689dc87ab0fcd0d
mizuho.otani <ruisui@gmail.com>
2026-02-22 08:44:23 +0900

    .c-card-archive-containerにcounter-incrementを設定しました。

--- front-page.php
+++ front-page.php
@@ -5,7 +5,8 @@
   <section class="top-kv">
     <div class="top-kv-container l-container">
       <div class="top-kv-text-content">
-        <img class="top-kv-emblem" src="<?php echo esc_url( get_template_directory_uri() . '/img/top/top-kv-emblem.webp' ); ?>" width="465"
+        <img class="top-kv-emblem"
+          src="<?php echo esc_url( get_template_directory_uri() . '/img/top/top-kv-emblem.webp' ); ?>" width="465"
           height="151" alt="制作会社の掲載数ナンバーワン" decoding="async" />
         <h2 class="top-kv-title">
           <span class="top-kv-title--green"><span>Web</span>制作会社</span><span class="top-kv-title--black">への転職<span
@@ -426,55 +427,58 @@
     </article>
     <!-- 求人一覧カード_ベース end -->
     <!-- 求人一覧カード_人気ランキング -->
-    <article class="c-card-archive-container">
-      <div class="c-card-archive-rank-view-container">
-        <div class="c-card-archive-rank c-card-archive-rank--top">1位</div>
-        <div class="c-card-archive-views"><span class="c-card-archive-views-number">X</span>Views</div>
-        <div class="c-card-archive-image">
-          <img src="<?php echo esc_url( get_template_directory_uri() . '/img/sub/recruite-card-thumbnail@2x.webp' ); ?>"
-            width="1548" height="602" alt="合同会社LIBERA_求人用サムネイル" decoding="async" />
+    <section class="popular-ranking-section ranking-counter-reset">
+      <article class="c-card-archive-container ranking-counter-increment">
+        <div class="c-card-archive-rank-view-container">
+          <div class="c-card-archive-rank c-card-archive-rank--top"></div>
+          <div class="c-card-archive-views"><span class="c-card-archive-views-number">X</span>Views</div>
+          <div class="c-card-archive-image">
+            <img
+              src="<?php echo esc_url( get_template_directory_uri() . '/img/sub/recruite-card-thumbnail@2x.webp' ); ?>"
+              width="1548" height="602" alt="合同会社LIBERA_求人用サムネイル" decoding="async" />
+          </div>
         </div>
-      </div>
-      <div class="c-card-archive-content">
-        <div class="c-card-archive-header">
-          <img class='c-card-archive-company-logo'
-            src='<?php echo esc_url( get_template_directory_uri() . '/img/common/company-logo@2x.webp' ); ?>'
-            width='120' height='120' alt='合同会社LIBERAロゴ' decoding='async' />
-          <h3 class="c-card-archive-company">合同会社LIBERA</h3>
+        <div class="c-card-archive-content">
+          <div class="c-card-archive-header">
+            <img class='c-card-archive-company-logo'
+              src='<?php echo esc_url( get_template_directory_uri() . '/img/common/company-logo@2x.webp' ); ?>'
+              width='120' height='120' alt='合同会社LIBERAロゴ' decoding='async' />
+            <h3 class="c-card-archive-company">合同会社LIBERA</h3>
+          </div>
+          <p class="c-card-archive-copy">【未経験OK】Web制作会社のコーダー募集｜デザインの意図を形にする仕事</p>
+          <ul class="c-card-archive-tags">
+            <li><a href="/">#未経験歓迎</a></li>
+            <li><a href="/">#リモート可</a></li>
+            <li><a href="/">#副業OK</a></li>
+            <li><a href="/">#フレックス勤務</a></li>
+            <li><a href="/">#経験者優遇</a></li>
+            <li><a href="/">#服装自由</a></li>
+            <li><a href="/">#土日休み</a></li>
+            <li><a href="/">#PC支給</a></li>
+          </ul>
+          <div class="c-card-archive-list-container">
+            <dl class="c-card-archive-list">
+              <dt class="c-card-archive-list-title">雇用形態</dt>
+              <dd class="c-card-archive-list-value">正社員</dd>
+              <dt class="c-card-archive-list-title">職種</dt>
+              <dd class="c-card-archive-list-value">コーダー・エンジニア</dd>
+              <dt class="c-card-archive-list-title">地域</dt>
+              <dd class="c-card-archive-list-value">福岡</dd>
+              <dt class="c-card-archive-list-title">年収</dt>
+              <dd class="c-card-archive-list-value">300万円</dd>
+            </dl>
+          </div>
         </div>
-        <p class="c-card-archive-copy">【未経験OK】Web制作会社のコーダー募集｜デザインの意図を形にする仕事</p>
-        <ul class="c-card-archive-tags">
-          <li><a href="/">#未経験歓迎</a></li>
-          <li><a href="/">#リモート可</a></li>
-          <li><a href="/">#副業OK</a></li>
-          <li><a href="/">#フレックス勤務</a></li>
-          <li><a href="/">#経験者優遇</a></li>
-          <li><a href="/">#服装自由</a></li>
-          <li><a href="/">#土日休み</a></li>
-          <li><a href="/">#PC支給</a></li>
-        </ul>
-        <div class="c-card-archive-list-container">
-          <dl class="c-card-archive-list">
-            <dt class="c-card-archive-list-title">雇用形態</dt>
-            <dd class="c-card-archive-list-value">正社員</dd>
-            <dt class="c-card-archive-list-title">職種</dt>
-            <dd class="c-card-archive-list-value">コーダー・エンジニア</dd>
-            <dt class="c-card-archive-list-title">地域</dt>
-            <dd class="c-card-archive-list-value">福岡</dd>
-            <dt class="c-card-archive-list-title">年収</dt>
-            <dd class="c-card-archive-list-value">300万円</dd>
-          </dl>
+        <div class="c-card-link-container c-card-link-recruit-container">
+          <a class="c-card-link c-card-link-apply" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+            aria-label="求人に応募する">応募する</a>
+          <a class="c-card-link c-card-link-view-more" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+            aria-label="求人の詳細を見る">詳しく見る</a>
+          <a class="c-card-link c-card-link-favorite" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
+            aria-label="求人募集記事をお気に入り登録する">お気に入り</a>
         </div>
-      </div>
-      <div class="c-card-link-container c-card-link-recruit-container">
-        <a class="c-card-link c-card-link-apply" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
-          aria-label="求人に応募する">応募する</a>
-        <a class="c-card-link c-card-link-view-more" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
-          aria-label="求人の詳細を見る">詳しく見る</a>
-        <a class="c-card-link c-card-link-favorite" href="<?php echo esc_url( home_url( '/recruit/1/' ) ); ?>"
-          aria-label="求人募集記事をお気に入り登録する">お気に入り</a>
-      </div>
-    </article>
+      </article>
+    </section>
     <!-- 求人一覧カード_お気に入り登録 end -->
     <!-- 下層ページ用コラム一覧カード -->
     <article class="c-card-column-container">
