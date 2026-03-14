ac06f6778346d8c67595051aff31fb60eea5fe73
mizuho.otani <ruisui@gmail.com>
2026-03-14 20:40:54 +0900

    絞り込み検索部分のロジックをfunctions.phpに記述しました。

--- template-parts/search-form.php
+++ template-parts/search-form.php
@@ -1,5 +1,5 @@
       <!-- 絞り込み検索 -->
-      <form class="c-search-form" action="<?php echo esc_url( home_url( '/recruit/search/' ) ); ?>" method="get">
+      <form class="c-search-form" action="<?php echo esc_url( home_url( '/recruit/' ) ); ?>" method="get">
         <details class="c-search-form-details js-search-form-details">
           <summary class="c-search-form-summary js-search-form-summary">
             絞り込み検索
@@ -14,7 +14,7 @@
               <label class="c-search-form-label c-search-form-label--area" for="area">地域</label>
               <div class="c-search-form-select-wrapper">
                 <select class="c-search-form-input" name="area" id="area">
-                  <option value="" disabled selected>選択してください</option>
+                  <option value="">選択してください</option>
                   <?php
                   $parent_areas = get_terms( array(
                     "taxonomy" => "area",
@@ -54,7 +54,7 @@
               <label class="c-search-form-label c-search-form-label--employment-type" for="employment_type">雇用形態</label>
               <div class="c-search-form-select-wrapper">
                 <select class="c-search-form-input" name="employment_type" id="employment_type">
-                  <option value="" disabled selected>選択してください</option>
+                  <option value="">選択してください</option>
                   <?php
                   $employment_terms = get_terms( array(
                     "taxonomy" => "employment_type",
@@ -79,7 +79,7 @@
               <label class="c-search-form-label c-search-form-label--job-category" for="job_category">職種</label>
               <div class="c-search-form-select-wrapper">
                 <select class="c-search-form-input" name="job_category" id="job_category">
-                  <option value="" disabled selected>選択してください</option>
+                  <option value="">選択してください</option>
                   <?php
                   $job_categories = get_terms( array(
                     "taxonomy" => "job_category",
@@ -105,10 +105,12 @@
                   class="c-search-form-label-salary-inner">年収<span>（単位：万円）</span></span></span>
               <div class="c-search-form-salary-wrapper">
                 <input class="c-search-form-input c-search-form-salary--min" type="number" name="salary_min"
-                  id="salary_min" placeholder="例：300">
+                  id="salary_min" placeholder="例：300" step="50" min="0"
+                  value="<?php echo isset( $_GET['salary_min'] ) ? esc_attr( $_GET['salary_min'] ) : ''; ?>">
                 <span class="c-search-form-label-separator">~</span>
                 <input class="c-search-form-input c-search-form-salary--max" type="number" name="salary_max"
-                  id="salary_max" placeholder="例：500">
+                  id="salary_max" placeholder="例：500" step="50" min="0"
+                  value="<?php echo isset( $_GET['salary_max'] ) ? esc_attr( $_GET['salary_max'] ) : ''; ?>">
               </div>
             </div>
             <!-- ボタン -->
