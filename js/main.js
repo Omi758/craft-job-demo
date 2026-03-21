7c873e33cb47480f8e81146f831191e53c915d7b
mizuho.otani <ruisui@gmail.com>
2026-03-21 22:16:18 +0900

    search-form-accordion.jsを作成し、search-formの開閉とアニメーションを記述しました

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { initializeSwitchViewport } from "./component/switch-viewport.js";
 import { initializeTopSlider } from "./component/top-slider.js";
+import { initializeSearchFormAccordion } from "./component/search-form-accordion.js";
 
 // 全ての初期化を実行
 initializeSwitchViewport();
 initializeTopSlider();
+initializeSearchFormAccordion();
