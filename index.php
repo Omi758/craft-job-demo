6257f83f6c67ce9aa8a2d5df0ea48e9a41621f61
mizuho.otani <ruisui919@gmail.com>
2026-03-25 06:13:29 +0900

    index.phpを記述しました

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<?php get_template_part('template-parts/breadcrumb'); ?>
+
+<main class="page-404 u-mtb-page">
+  <div class="l-container-s">
+    <div class="c-page-title-container">
+      <h1 class="c-page-title">ページが見つかりません</h1>
+    </div>
+
+    <div class="page-404-text-container">
+      <p class="page-404-text">お探しのページは見つかりませんでした。</p>
+      <p class="page-404-subtext">
+        <span>入力したアドレスが間違っているか、ページが移動・削除された可能性があります。</span>
+        <span>トップページに戻って目的の情報をお探し下さい。</span>
+      </p>
+    </div>
+
+    <div class="c-button-link-container">
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="c-button-link c-button-link--home">トップページへ戻る</a>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
