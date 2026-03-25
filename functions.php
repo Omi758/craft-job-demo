9129cd73f5e89aba0eba26043a91292b493e4625
mizuho.otani <ruisui@gmail.com>
2026-03-26 06:53:04 +0900

    URLのリライトルール修正&追記しました

--- functions.php
+++ functions.php
@@ -363,6 +363,17 @@ function craftjob_breadcrumb_category_url( $url, $type, $id ) {
 }
 add_filter( 'bcn_breadcrumb_url', 'craftjob_breadcrumb_category_url', 10, 3 );
 
+/**
+ * パンくずの「求人」を「求人を探す」に変更
+ */
+function craftjob_breadcrumb_rename_recruit( $title, $type, $id ) {
+	if ( $type[0] === 'archive' && $title === '求人' ) {
+			$title = '求人を探す';
+	}
+	return $title;
+}
+add_filter( 'bcn_breadcrumb_title', 'craftjob_breadcrumb_rename_recruit', 10, 3 );
+
 
 	/**
  * ブロックエディタ_カスタムブロックスタイル（もくじ）
